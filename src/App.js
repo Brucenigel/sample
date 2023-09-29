8e63cd5be0f6c3373420df2dab94904309bc6e47
Brucenigel <brucenige.vilo@sun-asterisk.com>
2023-09-29 11:47:19 +0800

    add number

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const add = () =>{ 
+     return 1 + 1
+  }
   return (
     <div className="App">
       <header className="App-header">
