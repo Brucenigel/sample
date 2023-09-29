05f770de6e76f8ed45fbef23b00bc040fba27b4e
Brucenigel <brucenige.vilo@sun-asterisk.com>
2023-09-29 11:54:02 +0800

    test2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-
-  const add = () =>{ 
-     return 1 + 1
-  }
+  const add = () => {
+    return 1 + 1;
+  };
   return (
     <div className="App">
       <header className="App-header">
+        { add() }
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
